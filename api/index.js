894ea2df616028a1099329cf1edbb398769bda9c
arexdu <710776009@qq.com>
2017-08-12 20:39:34 +0800

    fetch from api

--- api/index.js
+++ api/index.js
@@ -2,9 +2,20 @@ import express from 'express';
 import data from '../src/testData.json';
 
 const router = express.Router();
+const contests = data.contests.reduce((obj, contest) => {
+    obj[contest.id] = contest;
+    return obj;
+}, {});
 
 router.get('/contests', (req,res) => {
-    res.send({ contests: data.contests });
+    res.send({ contests:  contests });
 });
+
+router.get('/contests/:contestId', (req,res) => {
+    let contest = data.contests[req.params.contestId - 1];
+    contest.description = 'Lorm skd  ldld dsd kasdkf. Kaep kxoe ope. Pkd eopolcm qpdk kxnd. Pkdk enady iwndn.';
+    res.send(contest);
+});
+
 
 export default router;
--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from './Header';
 import ContestList from './ContestList';
+import Contest from './Contest';
 import axios from 'axios';
+import * as api from '../api';
 
 const pushState = (obj, url) =>
     window.history.pushState(obj, '', url);
@@ -35,15 +37,34 @@ class App extends React.Component {
             { currentContestId: contestId },
             `/contest/${contestId}`
         );
+        //look up contests
+        api.fetchContest(contestId).then(contest => {
+            this.setState({
+                pageHeader: contest.contestName,
+                currentContestId: contest.id,
+                contests: {
+                    ...this.state.contests,
+                    [contest.id]: contest
+                }
+            });
+        });
     };
+
+    currentContest() {
+        if (this.state.currentContestId) {
+            return <Contest {...this.state.contests[this.state.currentContestId]} />;
+        }
+
+        return <ContestList
+                onContestClick={this.fetchContest}
+                contests={this.state.contests} />;
+    }
 
     render() {
         return (
             <div className="App">
                 <Header message={this.state.pageHeader}/>
-                <ContestList
-                    onContestClick={this.fetchContest}
-                    contests={this.state.contests} />
+                {this.currentContest()}
             </div>
         )
 
